676979afbff7a52dff725765eac379e89c99f822
Tanner Saint <tsaint2004@gmail.com>
2025-10-23 03:02:59 +0000

    added add-post content with image picker

--- app/(tabs)/add-post.tsx
+++ app/(tabs)/add-post.tsx
@@ -1,9 +1,117 @@
-import { Text, View } from "react-native";
+import { useImagePicker } from "@/hooks/useImagePicker";
+import { SymbolView } from "expo-symbols";
+import { Image } from "expo-image";
+import { Button, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
 
 export default function Page() {
+    const {image, openImagePicker, reset} = useImagePicker();
+    const [caption, setCaption] = useState("");
+    
     return (
-        <View>
-            <Text>Add Post</Text>
+        <View style={styles.container}>
+            <Image
+                style={styles.imagePreview}
+                source={image ? { uri: image} : require("@/assets/images/placeholder.png")}
+                contentFit="cover"
+            />
+            <View style={styles.container}>
+                {!image && (
+                    <Pressable style={styles.choosePhotoButton} onPress={openImagePicker}>
+                        <View style={styles.buttonContent}>
+                            <SymbolView name="photo" tintColor={"#FFFFFF"}/>
+                            <Text style={styles.buttonText}>Choose a photo</Text>
+                        </View>
+                    </Pressable>
+                )}
+                {image && (
+                    <View style={styles.container}>
+                        <TextInput
+                            style={styles.captionInput}
+                            value={caption}
+                            onChangeText={setCaption}
+                            placeholder="Add a caption"
+                            placeholderTextColor="#00d6ad"
+                        />
+                        <Pressable style={styles.savePhotoButton} onPress={() => alert("Post added!")}>
+                            <View style={styles.buttonContent}>
+                                <Text style={styles.buttonText}>Save</Text>
+                            </View>
+                        </Pressable>
+                        <Pressable style={styles.resetButton} onPress={reset}>
+                            <Text style={styles.resetButtonText}>Reset</Text>
+                        </Pressable>
+                        <View style={styles.spacer}></View>
+                    </View>
+                )}
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        flex: 1,
+        alignItems: "center"
+    },
+    imagePreview: {
+        width: "80%",
+        aspectRatio: 1,
+        borderRadius: 24,
+        marginTop: 40,
+    },
+    choosePhotoButton: {
+        width: "80%",
+        height: 50,
+        backgroundColor: "#00d6ad",
+        borderRadius: 16,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 24,
+    },
+    buttonContent: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 8,
+    },
+    buttonText: {
+        color: "white"
+    },
+    captionInput: {
+        width: "80%",
+        height: 50,
+        borderWidth: 1,
+        borderColor: "#00d6ad",
+        borderRadius: 8,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 24,
+        paddingHorizontal: 8,
+    },
+    savePhotoButton: {
+        width: "70%",
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 16,
+        backgroundColor: "#00d6ad",
+        marginTop: 24,
+    },
+    resetButton: {
+        width: "70%",
+        flex: 1,
+        borderRadius: 16,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    resetButtonText: {
+        color: "black",
+        textAlign: "center",
+    },
+    spacer: {
+        width: "100%",
+        height: 85,
+    },
+});
---
+++ hooks/useImagePicker.ts
@@ -0,0 +1,35 @@
+import { usePermissions } from "expo-media-library";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+
+export function useImagePicker() {
+    const [image, setImage] = useState<string | undefined>(undefined);
+    const [status, requestPermission] = usePermissions();
+
+    async function openImagePicker() {
+        if (status === null) {
+            const permission = await requestPermission();
+            if (permission.granted === false) {
+                alert("Lumigram requires permission to access your photos");
+                return;
+            }
+        }
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+        });
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+
+    }
+
+    function reset() {
+        setImage(undefined);
+        return;
+    }
+
+    return {image, openImagePicker, reset};
+}
